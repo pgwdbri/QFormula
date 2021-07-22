aef5045cec7fd95c7e016e4f95a67fe41998c848
Brianna Shade <77507157+pgwdbri@users.noreply.github.com>
2021-07-21 22:32:20 -0400

    Add files via upload

---
+++ QuadraticFormula.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+from math import sqrt
+a = 2; b = 10; c = 2
+q = (b**2)-(4*a*c)
+x1 = (-b + sqrt(q))/(2*a)
+x2 = (-b - sqrt(q))/(2*a)
+print (x1)
+print (x2)
+
+
